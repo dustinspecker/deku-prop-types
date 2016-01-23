982c46fa79e3b8935b836265cb569b4c6d86cfc5
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-23 17:33:02 -0600

    refactor(index): create checker-factory file

---
+++ src/checker-factory.js
@@ -0,0 +1,22 @@
+module.exports = (name, validator) => ({
+  get isRequired() {
+    this.required = true
+    return this
+  },
+  validate(prop, key) {
+    if (this.required && prop === undefined) {
+      return new Error(`${key} is required`)
+    }
+
+    if (prop !== undefined) {
+      if (typeof validator === 'string' && typeof prop !== validator) {
+        const actualType = typeof prop
+        return new TypeError(`Expected ${key} to be of type \`${name}\`, but got \`${actualType}\``)
+      }
+
+      if (typeof validator === 'function') {
+        return validator(prop, key)
+      }
+    }
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-const checkerFactory = (name, validator) => ({
-  get isRequired() {
-    this.required = true
-    return this
-  },
-  validate(prop, key) {
-    if (this.required && prop === undefined) {
-      return new Error(`${key} is required`)
-    }
-
-    if (prop !== undefined) {
-      if (typeof validator === 'string' && typeof prop !== validator) {
-        const actualType = typeof prop
-        return new TypeError(`Expected ${key} to be of type \`${name}\`, but got \`${actualType}\``)
-      }
-
-      if (typeof validator === 'function') {
-        return validator(prop, key)
-      }
-    }
-  }
-})
+import checkerFactory from './checker-factory'
 
 module.exports.propTypes = {
   get any() {
